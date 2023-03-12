abea78fc09e0da88376cbe0abac205bd8075f2ba
dharamveer <dharamveer.gla_cs20@gla.ac.in>
2023-03-12 22:04:06 +0530

    Added os CoreModule

---
+++ 01nodejsCoreModule/Math.js
@@ -0,0 +1,6 @@
+const add = (a,b)=> a+b;
+const sub = (a,b)=>a-b;
+const mul = (a,b)=>a*b;
+const div =(a,b)=>a/b;
+
+module.exports = {add,sub,mul,div}   // The exports is an object. So, you can attach properties or methods to it
